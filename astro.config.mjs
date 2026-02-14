b1dbad388c903e5976c68dea1d7e6dee9c6fc62c
LeNot-T <axleyend@gmail.com>
2026-02-14 13:25:57 -0500

    added site configuration

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,7 @@ import { defineConfig, envField } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://LeNot-T.github.com',
+  site: 'https://lenott.github.com',
   integrations: [react()],
   adapter: netlify(),
   env: {
